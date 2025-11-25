2c5a20c684626d472367fcf69b06f698932a1a1e
ouerdia-zouba <ouerdia.zouba@gmail.com>
2025-11-25 17:25:23 +0100

    clicked

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,31 @@
+
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/ui/Button/Button";
 
 function App() {
+  const [counter, setCounter] = useState(-1);
+  useEffect(() => {
+    console.log('dans la fonction useEffect' ,counter);
+    return () =>{
+
+    };
+  }, [counter])
+
+  useEffect(() => {
+    setCounter(0);
+    }, [])
+
   return (
     <div>
+      counter: {counter}
       <Button
         type="button"
         color="skyblue"
         onButtonClick={() => {
           console.log("-1 click");
+          setCounter(counter-1);
+          console.log(counter);
         }}
       >
         -1
@@ -18,7 +35,10 @@ function App() {
         color="aquamarine"
         onButtonClick={() => {
           console.log("+1 click");
-        }}
+          setCounter(counter+1);
+          console.log(counter);
+        }
+      }
       >
         +1
       </Button>
--- src/App/components/ui/Button/Button.tsx
+++ src/App/components/ui/Button/Button.tsx
@@ -1,19 +1,37 @@
 import type React from "react";
 import style from "./Button.module.css";
+import { useEffect, useState } from "react";
 interface IButtonProps {
   // titre: string;
   color?: string;
   type?: "reset" | "submit" | "button";
   children: React.ReactNode | string | Array<React.ReactElement | string>;
+
   onButtonClick: () => void;
 }
-const Button: React.FC<IButtonProps> = ({ type, children, color, onButtonClick }) => {
+const Button: React.FC<IButtonProps> = ({
+  type,
+  children,
+  color,
+  onButtonClick,
+}) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    if (isClicked) {
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 350);
+    }
+  }, [isClicked]);
+
   return (
     <button
       type={type}
       style={{ backgroundColor: color }}
-      className={style.Button}
+      className={style.Button + (isClicked ? " " + style.clicked : "")}
       onClick={() => {
+        setIsClicked(true);
         onButtonClick();
       }}
     >
