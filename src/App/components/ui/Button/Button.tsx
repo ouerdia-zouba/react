6ff5bdd5779a89f0dada0cffafaf7561227dd218
ouerdia-zouba <ouerdia.zouba@gmail.com>
2025-11-25 14:58:08 +0100

    style

--- src/App/components/ui/Button/Button.tsx
+++ src/App/components/ui/Button/Button.tsx
@@ -1,11 +1,13 @@
-
+import type React from "react"
+import style from './Button.module.css'
 interface IButtonProps {
     titre: string;
     color?: string;
     type?: "reset" | "submit" | "subuttonbmit"; 
+    children: React.ReactNode|string|Array<React.ReactElement|string>;
 }
-const  Button=({titre='button'}: IButtonProps) => {
-    console.log(titre);
-    return <div>{titre}</div>
+const  Button:React.FC<IButtonProps> = ({type='button', children}) => {
+    
+    return <div className={style.Button}>{children}</div>
 };
 export default Button;
