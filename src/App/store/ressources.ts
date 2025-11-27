b94b3a446efe1614f1d55ea17b149e0d24313c02
ouerdia-zouba <ouerdia.zouba@gmail.com>
2025-11-27 14:49:25 +0100

    ressourcesReducer

--- src/App/store/ressources.ts
+++ src/App/store/ressources.ts
@@ -1,29 +1,36 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import type { ImageInterface, MemeInterface } from 'orsys-tjs-meme';
-import { REST_ADR, REST_RESSOURCES } from '../constantes/config';
-interface IRessourcesState{
-    memes:Array<MemeInterface>;
-    images: Array<ImageInterface>
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import type { ImageInterface, MemeInterface } from "orsys-tjs-meme";
+import { REST_ADR, REST_RESSOURCES } from "../constantes/config";
+interface IRessourcesState {
+  memes: Array<MemeInterface>;
+  images: Array<ImageInterface>;
 }
 
 const initialState: IRessourcesState = {
-    memes:[],
-    images:[]
-
-}
+  memes: [],
+  images: [],
+};
 
 const ressources = createSlice({
-  name: 'ressources',
+  name: "ressources",
   initialState,
-  reducers: {}
+  reducers: {},
+  //ecouter tous ce qui se passe
+  extraReducers(builder) {
+    builder.addCase(loadRessources.fulfilled, (state, action) => {
+      state.memes = action.payload.memes;
+      state.images = action.payload.images;
+    });
+  },
 });
 
 //export const {} = ressources.actions
-export const loadRessources=createAsyncThunk('ressources/load',async()=>{
-    const prm=(await fetch(`${REST_ADR}${REST_RESSOURCES.memes}`));
-     const pri=(await fetch(`${REST_ADR}${REST_RESSOURCES.images}`));
-     const prAll=await Promise.all([prm, pri]);
-    return {memes: await prAll[0].json(), images: await prAll[1].json()};
+export const loadRessources = createAsyncThunk("ressources/load", async () => {
+  const prm = await fetch(`${REST_ADR}${REST_RESSOURCES.memes}`);
+  const pri = await fetch(`${REST_ADR}${REST_RESSOURCES.images}`);
+  const prAll = await Promise.all([prm, pri]);
+  return { memes: await prAll[0].json(), images: await prAll[1].json() };
 });
 
-export default ressources.reducer
+ const ressourcesReducer=ressources.reducer;
+export default ressourcesReducer
